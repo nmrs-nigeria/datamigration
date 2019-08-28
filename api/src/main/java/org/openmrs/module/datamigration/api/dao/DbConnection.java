732e56ad8b390e004a830babe64c4c30ca1d453d
dkayiwa <kayiwadaniel@gmail.com>
2019-08-28 14:46:34 +0300

    Removing the hard coding of database connection information

--- api/src/main/java/org/openmrs/module/datamigration/api/dao/DbConnection.java
+++ api/src/main/java/org/openmrs/module/datamigration/api/dao/DbConnection.java
@@ -14,8 +14,9 @@ public class DbConnection {
 		
 		try {
 			NmrsConnection nmrsConnection = OptionsUtils.getNmrsConnectionDetails(props);
-			connection = (Connection) DriverManager.getConnection(nmrsConnection.getUrl().replace("twopointtow", "apindb"),
-			    "root", "P@ssw0rd");
+			connection = (Connection) DriverManager.getConnection(nmrsConnection.getUrl(),
+					nmrsConnection.getUsername(), nmrsConnection.getPassword());
+			
 			return connection;
 		}
 		catch (Exception e) {
