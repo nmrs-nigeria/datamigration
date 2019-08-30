6d87b84784bde3c8062e37c3be0cfe13603c5224
Johnbosco <multiplechuks@yahoo.com>
2019-08-30 11:34:25 +0000

    More changes for facility Resources

--- api/src/main/java/org/openmrs/module/datamigration/util/LocationUtil.java
+++ api/src/main/java/org/openmrs/module/datamigration/util/LocationUtil.java
@@ -10,11 +10,12 @@ public abstract class LocationUtil {
 	
 	public static Location InsertLocation(Facility facility/*, Address address*/) {
 		try {
-			Location location = null;
-			location = new Location();
+			Location location = new Location();
 			location.setAddress1(facility.getFacilityName());
 			location.setName(facility.getFacilityName());
 			location.setCountry("Nigeria");
+			location.setCityVillage(facility.getLga());
+			location.setCountyDistrict(facility.getState());
 			
 			//check if location exists
 			Location l = Context.getLocationService().getLocation(location.getName());
