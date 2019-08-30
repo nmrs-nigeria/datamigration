7e6f463cfbfcfa13aab35f783f24cb0bb728b157
Johnbosco <multiplechuks@yahoo.com>
2019-08-30 14:14:07 +0000

    Some minor change

--- api/src/main/java/org/openmrs/module/datamigration/util/PatientUtil.java
+++ api/src/main/java/org/openmrs/module/datamigration/util/PatientUtil.java
@@ -7,6 +7,7 @@ import org.openmrs.PersonName;
 import org.openmrs.api.context.Context;
 import org.openmrs.module.datamigration.util.Model.Migration;
 
+import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.Set;
 import java.util.TreeSet;
@@ -15,6 +16,8 @@ public abstract class PatientUtil {
 	
 	public static Patient InsertPatient(Migration delegate, Location location) {
 		try {
+			
+			SimpleDateFormat dateFormat = new SimpleDateFormat("dd/MM/yyyy");
 			Patient patient = new Patient();
 			//handle patient identifiers
 			Set<PatientIdentifier> patientIdentifiers = new TreeSet<PatientIdentifier>();
@@ -30,11 +33,14 @@ public abstract class PatientUtil {
 			patient.setIdentifiers(patientIdentifiers);
 			
 			patient.addName(new PersonName(delegate.getGivenName(), delegate.getMiddleName(), delegate.getFamilyName()));
-			patient.setBirthdate(new Date());
-			patient.setGender("M");
-			
+			patient.setBirthdate(dateFormat.parse(delegate.getBirthDate()));
+			patient.setGender(delegate.getGender());
+			patient.setDead(Boolean.parseBoolean(delegate.getDead()));
+			patient.setDeathDate(dateFormat.parse(delegate.getDeathDate()));
+			//patient.setCauseOfDeath(delegate.getCauseOfDeath());
+
 			//check if the patient exists
-			Patient p = Context.getPatientService().getPatientByExample(patient);
+			/*Patient p = Context.getPatientService().getAllPatients().stream().;*/
 			//handle patient save to openmrs
 			Context.getPatientService().savePatient(patient);
 			return patient;
